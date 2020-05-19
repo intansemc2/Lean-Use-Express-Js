a9dccd77b6ab9dcdded8a17fb16cd429235c8dc3
intansemc2 <intansemc2@gmail.com>
2020-05-19 15:07:13 +0700

    Add Template engine

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const appliciation = express();
-const appliciationPort = 3000;
+const app = express();
+const appPort = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
  
-appliciation.get('/', function (request, response) {
-	response.send('Hello World');
+app.get('/', function (request, response) {
+	response.render('./index.pug',{
+		name: 'User'
+	});
+});
+
+app.get('/users', function (request, response) {
+	response.render('./users/index.pug',{
+		users: [
+			{ id: 0, name: "user 0" },
+			{ id: 1, name: "user 1" }
+		]
+	});
 });
  
-appliciation.listen(appliciationPort, function () {
-	console.log(`Server is on and listening at Port ${appliciationPort}`);
+app.listen(appPort, function () {
+	console.log(`Server is on and listening at Port ${appPort}`);
 });
