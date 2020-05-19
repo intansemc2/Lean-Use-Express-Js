11c788554e0be20d1cd4a7bba36762911d5d55df
intansemc2 <intansemc2@gmail.com>
2020-05-19 15:26:48 +0700

    Add Query parameters, User Pug for render html files

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const appPort = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
- 
+
+//Data
+var users = [
+	{ id: 0, name: "user 0" },
+	{ id: 1, name: "user 1" }
+];
+
+//Rounting 
 app.get('/', function (request, response) {
 	response.render('./index.pug',{
 		name: 'User'
@@ -13,13 +20,27 @@ app.get('/', function (request, response) {
 
 app.get('/users', function (request, response) {
 	response.render('./users/index.pug',{
-		users: [
-			{ id: 0, name: "user 0" },
-			{ id: 1, name: "user 1" }
-		]
+		users: users
 	});
 });
- 
+
+app.get('/users/search', function (request, response) {
+	var q = request.query.q;
+	var matchedUsers = [];
+
+	if (q) {
+		matchedUsers = users.filter(function (user) {
+			return user.name.indexOf(q) !== -1;
+		});
+	}
+
+	response.render('./users/index.pug',{
+		users: matchedUsers,
+		q: q
+	});
+});
+
+//Listen
 app.listen(appPort, function () {
 	console.log(`Server is on and listening at Port ${appPort}`);
 });
