d81d482479b5437482ccb39bba61c3684585efe3
intansemc2 <intansemc2@gmail.com>
2020-05-19 14:47:06 +0700

    Add port variable

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ appliciation.get('/', function (request, response) {
 	response.send('Hello World');
 });
  
-appliciation.listen(appliciationPort);
+appliciation.listen(appliciationPort, function () {
+	console.log(`Server is on and listening at Port ${appliciationPort}`);
+});
